1a89f49e731df73719b8aa9915021405701bb505
Urvino <castelanu91@gmail.com>
2023-10-11 12:19:57 -0700

    adjusted prompt to reflect max 100

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function changeSize() {
   if (newSize !== null) {
     newSize = parseInt(newSize);
     if (newSize < 1 || newSize > 100 || Number.isNaN(newSize)) {
-      alert("Enter a number from 1-64 range");
+      alert("Enter a number from 1-100 range");
       changeSize();
     } else {
       clearGrid();
